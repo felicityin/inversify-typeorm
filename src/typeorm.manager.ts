7508ff2ef0f9b0055800cda2ac1f5be24fe38136
PATHANAZAMKHAN <pathanazam001@gmail.com>
2025-08-30 10:25:10 +0530

    doc:Improved documentation for all over codebase

--- src/typeorm.manager.ts
+++ src/typeorm.manager.ts
@@ -14,8 +14,14 @@ export const container = new Container()
 
 export class TypeOrmManager {
   /**
-   * Bind DataSource and EntityManager to the container.
-   * @param options
+   * Binds a TypeORM DataSource and its corresponding EntityManager to a DI container.
+   *
+   * This function initializes a DataSource using the specified options and binds
+   * it along with its EntityManager to the DI container as constants.
+   *
+   * @param options - Configuration options for creating the DataSource.
+   * @param shouldInitialize - Determines whether the DataSource should be initialized immediately.
+   * @returns A Promise that resolves when DataSource and EntityManager are bound.
    */
   static async importRoot(options: TypeOrmOptions, shouldInitialize = true): Promise<void> {
     const dataSource = await createDataSource(options, shouldInitialize)
@@ -28,9 +34,15 @@ export class TypeOrmManager {
   }
 
   /**
-   * Binds the repository for each entity in `entities` to the container.
-   * @param entities
-   * @param dataSource
+   * Binds repositories for specified entities to a DI container.
+   *
+   * For each entity in the provided entities array, this function resolves
+   * the appropriate repository from the DataSource and binds it to the DI container
+   * as a constant value.
+   *
+   * @param entities - An array of entities whose repositories need to be bound.
+   * @param dataSource - The DataSource from which the repositories should be resolved. Defaults to the default data source name.
+   * @returns A Promise that resolves once all repositories are bound.
    */
   static async importRepository<Entity extends ObjectLiteral>(
     entities: EntityTarget<Entity>[],
@@ -46,11 +58,29 @@ export class TypeOrmManager {
     })
   }
 
+  /**
+   * Destroys an initialized DataSource, releasing all its resources.
+   *
+   * This function attempts to destroy a DataSource if it is initialized,
+   * thereby closing any connections and releasing resources tied to it.
+   *
+   * @param dataSource - The DataSource to be destroyed. Defaults to the default data source name.
+   * @returns A Promise that resolves when the DataSource is successfully destroyed.
+   */
   static async destroyDataSource(dataSource: CustomDataSource = DEFAULT_DATA_SOURCE_NAME): Promise<void> {
     const conn = this.getDataSource(dataSource)
     if (conn && conn.isInitialized) await conn.destroy()
   }
 
+  /**
+   * Retrieves a DataSource instance from the DI container.
+   *
+   * This function fetches a DataSource that has been previously bound
+   * to the DI container using its corresponding token.
+   *
+   * @param dataSource - The identifier for the DataSource to be retrieved. Defaults to the default data source name.
+   * @returns The DataSource instance corresponding to the specified identifier.
+   */
   static getDataSource(dataSource: CustomDataSource = DEFAULT_DATA_SOURCE_NAME): DataSource {
     const dataSourceToken = getDataSourceToken(dataSource)
     return container.get<DataSource>(dataSourceToken)
